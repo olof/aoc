5ddccce03b4fec10ebc67aa33bb38b5856e16598
Olof Johansson <olof@ethup.se>
2020-12-12 13:56:38 +0100

    dec12/2: improve code style

--- 2020/dec12/2.pl
+++ 2020/dec12/2.pl
@@ -10,11 +10,8 @@ my @pos = (0, 0);
 	E => sub { $wp[0] += pop },
 	W => sub { $wp[0] -= pop },
 	R => sub { unshift @wp, -pop @wp for 1 .. pop() / 90 },
-	L => sub { push @wp, -shift @wp for 1.. pop() / 90 },
-	F => sub {
-		$pos[0] += $_[0] * $wp[0];
-		$pos[1] += $_[0] * $wp[1];
-	},
+	L => sub { push @wp, -shift @wp for 1 .. pop() / 90 },
+	F => sub { $pos[$_] += $_[0] * $wp[$_] for 0, 1 }
 }->{$_->[0]}->($_->[1]) for @in;
 
 say abs($pos[0]) + abs($pos[1]);
