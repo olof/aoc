ad80d7fadfa04d7506923b0c299682fcf33a6bd3
Olof Johansson <olof@ethup.se>
2022-12-02 14:01:39 +0100

    2022: dec02: problem 2

---
+++ 2022/dec02/2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+perl -pe '
+	s/A X/A Z/ or
+	s/A Y/A X/ or
+	s/A Z/A Y/ or
+	s/B X/B X/ or
+	s/B Y/B Y/ or
+	s/B Z/B Z/ or
+	s/C X/C Y/ or
+	s/C Y/C Z/ or
+	s/C Z/C X/
+' | sh 1.sh
+
+# too high: 16171
