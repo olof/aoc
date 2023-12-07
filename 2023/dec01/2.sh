5e7e98e1ae1c9e5e980d662096ec4af8791df5cd
Olof Johansson <olof@ethup.se>
2023-12-07 02:12:16 +0100

    2023: dec01: problem 2
    Retake, after realize how slow my first (correct) attempt was.

--- 2023/dec01/2.sh
+++ 2023/dec01/2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 digitize() {
-  echo "$1" | sed -r \
+  sed -r \
     -e 's/(one|two|three|four|five|six|seven|eight|nine)/[\1]/g' \
     -e s/one/1/g -e s/two/2/g -e s/three/3/g \
     -e s/four/4/g -e s/five/5/g -e s/six/6/g \
@@ -9,15 +9,17 @@ digitize() {
 }
 
 rdigitize() {
-  echo "$1" | rev | sed -r \
+  rev | sed -r \
     -e 's/(enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)/[\1]/g' \
     -e s/eno/1/g -e s/owt/2/g -e s/eerht/3/g \
     -e s/ruof/4/g -e s/evif/5/g -e s/xis/6/g \
     -e s/neves/7/g -e s/thgie/8/g -e s/enin/9/g
 }
 
-echo $(( `while read line; do
-  printf "+%s%s\n" \
-    $(digitize "$line" | sed -re 's/^[^1-9]*([1-9]).*/\1/') \
-    $(rdigitize "$line" | sed -re 's/^[^1-9]*([1-9]).*/\1/')
-done` ))
+buf=`cat`
+buf1=`echo "$buf" | digitize`
+buf2=`echo "$buf" | rdigitize`
+echo $(( `
+  echo "$buf1" | sed -re 's/^[^1-9]*([1-9]).*/+\10/'
+  echo "$buf2" | sed -re 's/^[^1-9]*([1-9]).*/+\1/'
+` ))
