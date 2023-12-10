8f512838cf44f87a5d7a3094fde3e3e7dd493acc
Olof Johansson <olof@ethup.se>
2023-12-10 17:06:16 +0100

    Add common utility functions for shell solutions

---
+++ utils/utils.sh
@@ -0,0 +1,26 @@
+# Sourced shell utilities for aoc 2023
+# Generic things, instead of having to copy from previous solutions.
+
+count() {
+	echo $#
+}
+
+sum() {
+	local n=0
+	while read line; do
+		n=$((n+line))
+	done
+	echo $n
+}
+
+join() {
+	local delim="$1"
+	shift
+	[ "$#" -ge 1 ] || return 0
+	printf "%s" "$1"
+	shift
+	for arg in "$@"; do
+		printf "%s%s" "$delim" "$arg"
+	done
+	printf "\n"
+}
