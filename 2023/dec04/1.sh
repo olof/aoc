cb157776377e31fe50ce709f38e9581fadd9f1cb
Olof Johansson <olof@ethup.se>
2023-12-10 03:19:53 +0100

    2023: dec04: problem 1, reuse common functions

--- 2023/dec04/1.sh
+++ 2023/dec04/1.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
-sum() { n=0; while read line; do n=$((n+line)); done; echo $n; }
-
-join() {
-	local delim="$1"
-	shift
-	[ "$#" -ge 1 ] || return 0
-	printf "%s" "$1"
-	shift
-	for arg in "$@"; do
-		printf "%s%s" "$delim" "$arg"
-	done
-	printf "\n"
-}
-
-count() {
-	echo $#;
-}
-
+. utils.sh
 while read p p2 line; do
 	needles=${line% |*}
 	haystack=${line#*| }
